4680d932f239218108218cd0458f56cd07114ea5
max-wolf <zyfwol@hotmail.com>
2016-05-18 21:11:44 +0800

    collision2

--- collision2.cpp
+++ collision2.cpp
@@ -8,13 +8,16 @@
 #include "big.h"
 
 #define HASH_LEN 20 //sha-1 hash value 160bit
-#define SELECT_LEN 8    //the selected length(bit) of hash value
+#define SELECT_LEN 8    //the selected length(bit) of hash value to find collision
 #define HASH_MAX 10000 //the number of hash values
 #define FIR_LEN //first part of hash input length
 
 char alp[26];   //alphabet
 char* s = (char*) "ikkdtabyzhmknmwbwbkfrvwvttjvqd"; //first part of hash input
-char val[HASH_MAX][20]; //restore hash value
+//char val[HASH_MAX][20]; //restore hash value
+bitset<SELECT_LEN> bs[HASH_MAX];
+int bit_num = 0;
+int bit_num_now = 0;
 
 void sha_1(char* value, char* input, int len)
 {
@@ -25,6 +28,36 @@ void sha_1(char* value, char* input, int len)
         shs_process(&sh, input[i]);
     }
     shs_hash(&sh, value);
+}
+
+void val_to_bit(char value[], bitset<SELECT_LEN> &b)
+{
+    int char_num = SELECT_LEN / 8;
+    int char_mod = SELECT_LEN % 8;
+    bitset<8> tmp;
+    string str;
+    for(int i = 0; i < char_num + 1; ++i)
+    {
+        tmp = (int) value[i];
+        str = str + tmp.to_string();
+    }
+    bitset<SELECT_LEN> tmp2(str, 0, SELECT_LEN);
+    b = tmp2;
+}
+
+int find_collsion(int num, bitset<SELECT_LEN> b)
+{
+    string bi = b.to_string();
+    for(int j = 0; j < num; ++j)
+    {
+        string bj = bs[j].to_string();
+        if(bi.compare(bj) == 0)
+        {
+            cout<< "collision " << num << endl;
+            return num;
+        }
+    }
+    return -1;
 }
 
 void sort(vector<char> data, vector<char> target, int num)
@@ -39,7 +72,12 @@ void sort(vector<char> data, vector<char> target, int num)
             cc[i] = target[i];
         }
         strcat(ss, cc);
-
+        char val[20];
+        sha_1(val, ss, FIR_LEN+num);
+        val_to_bit(val, bs[bit_num]);
+        bit_num_now = bit_num;
+        bit_num++;
+        find_collsion(bit_num_now, bs[bit_num_now]);
         //cout<< ss << endl;
         return;
     }
