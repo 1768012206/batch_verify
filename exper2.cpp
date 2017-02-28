40c4e3875f4845cb03d122d02f4db76485680833
acer_lin <zyfwol@hotmail.com>
2017-02-28 20:37:26 +0800

    join_step1

--- exper2.cpp
+++ exper2.cpp
@@ -8,14 +8,14 @@
 #define MR_PAIRING_SSP
 #define AES_SECURITY 128
 
-#define N 10
+#define N 20
 
 #include "pairing_1.h"
 using namespace std;
+PFC pfc(AES_SECURITY);
 
 double ini_step1() {
     clock_t start, end;
-    PFC pfc(AES_SECURITY);
     Big ni,oi;
     pfc.random(ni);
     pfc.random(oi);
@@ -62,6 +62,74 @@ double ini_step1() {
     }
     end = clock();
     time= time + (double)(end-start)*1000/CLOCKS_PER_SEC;
+    return time;
+}
+
+double ini_step2() {
+    clock_t start, end;
+    G1 p0[N], p1[N], gpub, v, b[N], y[N], ai[N], fi;
+    pfc.random(gpub);
+    pfc.random(v);
+    pfc.random(fi);
+    for(int i = 0; i < N; i++) {
+        pfc.random(b[i]);
+        pfc.random(y[i]);
+        pfc.random(ai[i]);
+    }
+    Big El[N];
+    start = clock();
+    for(int i = 0; i < N; i++) {
+        pfc.hash_and_map(p0[i], (char*)"asdf0");
+        pfc.hash_and_map(p1[i], (char*)"asdf1");
+        El[i] = pfc.hash_to_group((char*)"asasdf");
+    }
+    G1 tmp0 = ai[0];
+    for(int i = 1; i < N; i++) {
+        tmp0 = tmp0 + ai[i];
+    }
+    G1 tmp1 = p0[0] + pfc.mult(p1[0], El[0]), tmp2;
+    for(int i = 1; i < N; i++) {
+        tmp2 = p0[i] + pfc.mult(p1[i], El[i]);
+        tmp1 = tmp1 + tmp2;
+    }
+    GT pair1 = pfc.pairing(tmp1, gpub);
+    G1 tmp3 = b[0];
+    for(int i = 1; i < N; i++) {
+        tmp3 = tmp3 + b[i];
+    }
+    GT pair2 = pfc.pairing(v, tmp3);
+    GT O = pair1 * pair2;
+    G1 tmp4 = y[0];
+    for(int i = 1; i < N; i++) {
+        tmp4 = tmp4 + y[i];
+    }
+    GT Li = pfc.pairing(fi, tmp0);
+    pfc.pairing(tmp4, gpub);
+    GT equ = O * Li;
+    end = clock();
+    double time=(double)(end-start)*1000/CLOCKS_PER_SEC;
+    return time;
+}
+
+double join_step1() {
+    clock_t start, end;
+    Big ni, oi, EI;
+    pfc.random(oi);
+    pfc.random(ni);
+    G1 v, fj[N], sI0, sI1;
+    pfc.random(sI0);
+    pfc.random(sI1);
+    start = clock();
+    pfc.hash_and_map(v, (char*)"asdf");
+    EI = pfc.hash_to_group((char*)"asdfqer");
+    for(int i = 0; i < N; i++) {
+        pfc.hash_and_map(fj[i], (char*)"asdfl");
+        G1 tmp1 = sI0 + pfc.mult(sI1, EI);
+        tmp1 = tmp1 + pfc.mult(v, oi);
+        tmp1 = tmp1 + pfc.mult(fj[i], ni);
+    }
+    end = clock();
+    double time=(double)(end-start)*1000/CLOCKS_PER_SEC;
     return time;
 }
 
